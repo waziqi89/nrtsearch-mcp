7aef2e2f6050f17e2736c12ed6a1aeaa3d51c156
waziqi89 <waziqi0809@gmail.com>
2025-06-25 16:47:45 -0400

    support sse

--- src/main/java/com/yelp/nrtsearch/nrtsearchmcp/NrtsearchMcpConfiguration.java
+++ src/main/java/com/yelp/nrtsearch/nrtsearchmcp/NrtsearchMcpConfiguration.java
@@ -3,6 +3,7 @@ package com.yelp.nrtsearch.nrtsearchmcp;
 import com.yelp.nrtsearch.nrtsearchmcp.client.NrtsearchClient;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,13 @@ import java.util.List;
 
 @Configuration
 public class NrtsearchMcpConfiguration {
+
+    @Value( "${nrtsearch.server.host:localhost}" )
+    private String host;
+    @Value("${nrtsearch.server.port:6000}")
+    private int port;
+
+
     @Bean
     public List<ToolCallback> tools(NrtsearchService nrtsearchService) {
         return List.of(
@@ -19,6 +27,6 @@ public class NrtsearchMcpConfiguration {
 
     @Bean
     public NrtsearchClient nrtsearchClient() {
-        return new NrtsearchClient("localhost", 6000);
+        return new NrtsearchClient(host, port);
     }
 }
